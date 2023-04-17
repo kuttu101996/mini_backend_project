6202579bf06f89849f90f8093957f98d6d5eb337
Snehasish Ghosh Dastidar <kuttulr1996@gmail.com>
2023-04-18 04:08:45 +0530

    cookie changes

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -1,16 +1,31 @@
 const jwt = require("jsonwebtoken")
-const cookieParser = require("cookie-parser")
+// const cookieParser = require("cookie-parser")
 const {client} = require("../routes/user.route")
 require("dotenv").config();
 
 const authentication = async(req,res,next)=>{
     try{
-        const userID = req.cookies.userID;
-        const token = req.cookies.token;
-        
+        // const userID = req.cookies.userID;
+        // const token = req.cookies.token;
+        let token = req.headers.authorization.split(' ')[1]
+        if (!token){
+            return res.status(400).send({message: "Please Login"})
+        }
+        jwt.verify(token, process.env.secretKey, (err, decoded)=>{
+            if (err){
+                return res.status.send({message: err})
+            }
+            req.body.userID = decoded.userID;
+            next()
+        })
     }
     catch(err){
         console.log("Authentication Catch block")
         res.status(500).send({message: "Something went wrong", err})
     }
+}
+
+
+module.exports = {
+    authentication
 }
